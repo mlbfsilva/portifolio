f009d6a652e7526bd1c9b47a2755b39bdef28884
Maria Luiza Baborsa <marialuiza.bf.silva@gmail.com>
2025-11-26 21:15:50 -0300

    fix: correção

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("downloadCV").addEventListener("click", function () {
     const link = document.createElement("a");
-    link.href = "arquivos/MARIA-LUIZA-BARBOSA-CV-Analista de Sistemas.pdf";
+    link.href = "files/MARIA-LUIZA-BARBOSA-CV-Analista de Sistemas.pdf";
     link.download = "MARIA-LUIZA-BARBOSA-CV-Analista de Sistemas.pdf";
     link.click();
 });
